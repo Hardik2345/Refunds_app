d202ab0fb2c2c903a2aa7c8e0da475cb3b2c2f20
Hardik Parikh <hardik@Hardiks-MacBook-Air-2.local>
2025-10-02 15:17:31 +0530

    inspecting redis keys

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,8 +1,11 @@
 const Redis = require("ioredis");
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
-const keys = await redis.keys("*"); // careful in prod
-console.log(keys);
+async function testRedisConnection() {
+  const keys = await redis.keys("*"); // careful in prod
+  console.log(keys);
+}
 
+testRedisConnection()
 
 module.exports = redis;
