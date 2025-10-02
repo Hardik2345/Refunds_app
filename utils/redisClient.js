28d8cb9cc2ae433066eb51c67a34cdcba731de39
Hardik Parikh <hardik@Hardiks-MacBook-Air-2.local>
2025-10-02 15:21:23 +0530

    reverting test change

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,11 +1,4 @@
 const Redis = require("ioredis");
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
-async function testRedisConnection() {
-  const keys = await redis.keys("*"); // careful in prod
-  console.log(keys);
-}
-
-testRedisConnection()
-
 module.exports = redis;
